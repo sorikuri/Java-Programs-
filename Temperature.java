297f900b042fd24567ea91f78f6c2439a086f262
sorikuri <sobicodo@gmail.com>
2026-05-16 17:32:53 +0500

    Temperature conversion

---
+++ Temperature.java
@@ -0,0 +1,13 @@
+// First Java Program for Temperture Conversion
+import java.util.Scanner;
+public class Temperature {
+    public static void main (String [] argus)
+    {
+        System.out.println ("Enter Temperature in Celcius : ");
+        Scanner C = new Scanner (System.in);
+        float c = C.nextFloat ();
+        float t = (c * 9/5) + 32 ; 
+        System.out.println ("Temperature in Farenhite : ");
+        System.out.print (t);
+    }
+}
